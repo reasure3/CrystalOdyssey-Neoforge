da1ad56b58d1fda2e03a38be73aba693bf2dbe82
reasure3 <reasure3@naver.com>
2025-01-02 16:53:22 +0900

    Increased loading stability by separating client-side curios code

--- src/main/kotlin/com/reasure/crystal_odyssey/client/CrystalOdysseyClient.kt
+++ src/main/kotlin/com/reasure/crystal_odyssey/client/CrystalOdysseyClient.kt
@@ -1,8 +1,7 @@
 package com.reasure.crystal_odyssey.client
 
 import com.reasure.crystal_odyssey.CrystalOdyssey
-import com.reasure.crystal_odyssey.client.curios.GlowstoneGemLanternCuriosRenderer
-import com.reasure.crystal_odyssey.item.ModItems
+import com.reasure.crystal_odyssey.client.curios.CuriosClientInitializer
 import net.neoforged.api.distmarker.Dist
 import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.ModList
@@ -11,12 +10,11 @@ import net.neoforged.fml.common.Mod
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
-import top.theillusivec4.curios.api.client.CuriosRendererRegistry
 
 @Mod(CrystalOdyssey.ID, dist = [Dist.CLIENT])
 @EventBusSubscriber(modid = CrystalOdyssey.ID, bus = EventBusSubscriber.Bus.MOD, value = [Dist.CLIENT])
 object CrystalOdysseyClient {
-    private val LOGGER: Logger = LogManager.getLogger("${CrystalOdyssey.ID}/client")
+    val LOGGER: Logger = LogManager.getLogger("${CrystalOdyssey.ID}/client")
 
     init {
         LOGGER.info("Starting initialization of ${CrystalOdyssey.ID}.client")
@@ -26,8 +24,7 @@ object CrystalOdysseyClient {
     private fun onClientSetup(event: FMLClientSetupEvent) {
         LOGGER.info("Starting clientSetup")
         if (ModList.get().isLoaded("curios")) {
-            LOGGER.info("Find Curios! Starting register Curios Renderer")
-            CuriosRendererRegistry.register(ModItems.GLOWSTONE_GEM_LANTERN, ::GlowstoneGemLanternCuriosRenderer)
+            CuriosClientInitializer.registerRenderer()
         }
     }
 }
---
+++ src/main/kotlin/com/reasure/crystal_odyssey/client/curios/CuriosClientInitializer.kt
@@ -0,0 +1,12 @@
+package com.reasure.crystal_odyssey.client.curios
+
+import com.reasure.crystal_odyssey.client.CrystalOdysseyClient
+import com.reasure.crystal_odyssey.item.ModItems
+import top.theillusivec4.curios.api.client.CuriosRendererRegistry
+
+object CuriosClientInitializer {
+    fun registerRenderer() {
+        CrystalOdysseyClient.LOGGER.info("Find Curios! Starting register Curios Renderer")
+        CuriosRendererRegistry.register(ModItems.GLOWSTONE_GEM_LANTERN, ::GlowstoneGemLanternCuriosRenderer)
+    }
+}

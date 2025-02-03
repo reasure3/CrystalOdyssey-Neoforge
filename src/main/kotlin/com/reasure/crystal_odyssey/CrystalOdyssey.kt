89f3f331b5b74c66d2910c7eef2aa438d702be15
reasure3 <reasure3@naver.com>
2025-02-03 23:29:45 +0900

    Fix Capability Register Bug

--- src/main/kotlin/com/reasure/crystal_odyssey/CrystalOdyssey.kt
+++ src/main/kotlin/com/reasure/crystal_odyssey/CrystalOdyssey.kt
@@ -7,28 +7,19 @@ import com.reasure.crystal_odyssey.inventory.menu.ModMenuTypes
 import com.reasure.crystal_odyssey.item.ModCreativeModTabs
 import com.reasure.crystal_odyssey.item.ModItems
 import com.reasure.crystal_odyssey.item.components.ModDataComponents
-import com.reasure.crystal_odyssey.item.fluid.InfiniteOneFluidHandlerItemStack
-import com.reasure.crystal_odyssey.item.fluid.OneFluidHandlerItemStack
 import com.reasure.crystal_odyssey.particle.ModParticleTypes
 import com.reasure.crystal_odyssey.recipe.ModRecipeSerializers
 import com.reasure.crystal_odyssey.recipe.ModRecipeTypes
 import net.minecraft.resources.ResourceLocation
-import net.minecraft.world.level.material.Fluids
-import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.ModContainer
 import net.neoforged.fml.ModList
-import net.neoforged.fml.common.EventBusSubscriber
 import net.neoforged.fml.common.Mod
 import net.neoforged.fml.config.ModConfig
-import net.neoforged.neoforge.capabilities.Capabilities
-import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent
-import net.neoforged.neoforge.fluids.FluidType
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import thedarkcolour.kotlinforforge.neoforge.forge.MOD_BUS
 
 @Mod(CrystalOdyssey.ID)
-@EventBusSubscriber(modid = CrystalOdyssey.ID, bus = EventBusSubscriber.Bus.MOD)
 class CrystalOdyssey(container: ModContainer) {
     companion object {
         const val ID = "crystal_odyssey"
@@ -56,32 +47,5 @@ class CrystalOdyssey(container: ModContainer) {
         if (ModList.get().isLoaded("curios")) {
             CuriosInitializer.init()
         }
-    }
-
-    @SubscribeEvent
-    private fun registerCapabilities(event: RegisterCapabilitiesEvent) {
-        event.registerItem(
-            Capabilities.FluidHandler.ITEM,
-            { stack, _ -> OneFluidHandlerItemStack(stack, Fluids.WATER, 30 * FluidType.BUCKET_VOLUME) },
-            ModItems.SAPPHIRE_BUCKET
-        )
-
-        event.registerItem(
-            Capabilities.FluidHandler.ITEM,
-            { stack, _ -> InfiniteOneFluidHandlerItemStack(stack, Fluids.WATER) },
-            ModItems.INFINITE_SAPPHIRE_BUCKET
-        )
-
-        event.registerItem(
-            Capabilities.FluidHandler.ITEM,
-            { stack, _ -> OneFluidHandlerItemStack(stack, Fluids.LAVA, 30 * FluidType.BUCKET_VOLUME) },
-            ModItems.RUBY_BUCKET
-        )
-
-        event.registerItem(
-            Capabilities.FluidHandler.ITEM,
-            { stack, _ -> InfiniteOneFluidHandlerItemStack(stack, Fluids.LAVA) },
-            ModItems.INFINITE_RUBY_BUCKET
-        )
     }
 }
